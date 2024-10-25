508507d4ac43c2589e382c094a94d8ee4bc6092e
muhammad-furqon <muhammad.furqon@cosmedia.co.jp>
2024-10-25 15:12:42 +0900

    Remove comment in backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -9,10 +9,3 @@ const backend = defineBackend({
   auth,
   data,
 });
-
-
-// const { groups } = backend.auth.resources
-
-
-// // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_iam.IRole.html
-// groups["ADMINS"].cfnUserGroup
